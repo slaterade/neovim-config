c70093f0f8ae7207f2407fcdf1840c765cf04e79
Richard Slater <richard@slaterade.com>
2022-11-13 13:49:50 -0800

    remove window movement keymaps

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,4 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
+-- packer managed plugins
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
