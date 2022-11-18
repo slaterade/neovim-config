c9eb1e748a5f97bc0cfd99ad72dd2c8d67505a4e
Richard Slater <richard@slaterade.com>
2022-11-17 19:48:37 -0800

    toggle for diagnostics

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>e", ":Ex<cr>")
 vim.keymap.set("n", "<leader>q", ":<cmd>qa<cr>")
 vim.keymap.set("n", "<leader>ts", "setlocal spell! spelllang=en_us<cr>")
 vim.keymap.set("n", "<leader>tt", ":ToggleTerm<cr>")
+vim.keymap.set("n", "<leader>ns", ":lua vim.diagnostic.show()<cr>")
+vim.keymap.set("n", "<leader>nh", ":lua vim.diagnostic.hide()<cr>")
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
