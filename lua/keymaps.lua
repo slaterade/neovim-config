75bbd377b16d475096035d937b6a64cf4c6a08a6
Richard Slater <richard@slaterade.com>
2022-12-19 21:22:42 -0800

    fix: refactor keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,27 +1,33 @@
--- normal mode keymaps
-local ks = vim.keymap.set
+local kmap = vim.keymap.set
 
-ks("n", "<leader>q", ":<cmd>qa<cr>")
-ks("n", "<leader>ee", ":Ex<cr>")
-ks("n", "<leader>cd", ":cd %:p:h<cr>:pwd<cr>")
-ks("n", "<leader>cs", ":let @/ = \"\"<cr>") -- clear search term
-ks("n", "<leader>ts", "setlocal spell! spelllang=en_us<cr>")
-ks("n", "<leader>tt", ":ToggleTerm<cr>")
-ks("n", "<leader>tb", ":GitBlameToggle<cr>")
-ks("n", "<c-d>", "<c-d>zz")
-ks("n", "<c-u>", "<c-u>zz")
-ks("n", "n", "nzz")
-ks("n", "N", "Nzz")
+-- normal mode keymaps
+kmap("n", "<leader>q", ":<cmd>qa<cr>")
+kmap("n", "<leader>ee", ":Ex<cr>")
+kmap("n", "<leader>cd", ":cd %:p:h<cr>:pwd<cr>")
+kmap("n", "<leader>cs", ":let @/ = \"\"<cr>") -- clear search term
+kmap("n", "<leader>ts", "setlocal spell! spelllang=en_us<cr>")
+kmap("n", "<leader>tt", ":ToggleTerm<cr>")
+kmap("n", "<leader>tb", ":GitBlameToggle<cr>")
+kmap("n", "<leader>gs", vim.cmd.Git)
+kmap("n", "<c-d>", "<c-d>zz")
+kmap("n", "<c-u>", "<c-u>zz")
+kmap("n", "n", "nzzzv")
+kmap("n", "N", "Nzzzv")
+kmap("n", "J", "mzJ`z")
 
 -- telescope
 local builtin = require('telescope.builtin')
-ks('n', '<leader>ff', builtin.find_files, {})
-ks('n', '<leader>fg', builtin.live_grep, {})
-ks('n', '<leader>fb', builtin.buffers, {})
-ks('n', '<leader>fh', builtin.help_tags, {})
+kmap('n', '<leader>ff', builtin.find_files, {})
+kmap('n', '<leader>fg', builtin.live_grep, {})
+kmap('n', '<leader>fb', builtin.buffers, {})
+kmap('n', '<leader>fh', builtin.help_tags, {})
+
+-- visual mode keymaps
+kmap("v", "J", ":m '>+1<cr>gv=gv")
+kmap("v", "K", ":m '<-2<cr>gv=gv")
 
 -- terminal mode keymaps
-ks("t", "<esc>", "<c-\\><c-n>")
+kmap("t", "<esc>", "<c-\\><c-n>")
 
 -- commands
 vim.cmd(':command! W w')
