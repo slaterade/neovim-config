a3139b67d2bfc71f429cd6756c2d96e43992028e
Richard Slater <richard@slaterade.com>
2023-07-11 14:44:02 -0700

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,40 +1,42 @@
-local lsp = require('lsp-zero')
+local lsp = require('lsp-zero').preset({})
 
-lsp.preset('recommended')
+-- local cmp = require('cmp')
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+--     ['<Left>'] = cmp.mapping.abort(),
+--     ['<Right>'] = cmp.mapping.complete(),
+--     --['<Tab>'] = cmp.mapping.confirm(),
+-- })
 
-lsp.ensure_installed({
-  'bashls',
-  'clangd',
-  'dockerls',
-  'eslint',
-  'lua_ls',
-  'marksman',
-  'pyright',
-  'tsserver',
-})
-
-local cmp = require('cmp')
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<Left>'] = cmp.mapping.abort(),
-  ['<Right>'] = cmp.mapping.complete(),
-  --['<Tab>'] = cmp.mapping.confirm(),
-})
-
--- disable completion with enter key tyvm
---cmp_mappings['<CR>'] = nil
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings,
-  completion = {
-    autocomplete = false
-  },
-})
+-- lsp.setup_nvim_cmp({
+--     mapping = cmp_mappings,
+--     completion = {
+--         autocomplete = false
+--     },
+-- })
 
 -- add keymaps only when attached to a language
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set('n', '<leader>lf', vim.cmd.LspZeroFormat, opts)
+    lsp.default_keymaps({ buffer = bufnr })
+    vim.keymap.set('v', '<Leader>lf', vim.cmd.LspZeroFormat, opts)
+    vim.keymap.set('n', '<leader>lf', vim.cmd.LspZeroFormat, opts)
+    vim.keymap.set('n', '<leader>ll', ":ClangdSwitchSourceHeader<cr>")
 end)
+
+-- lsp.setup_servers({
+--     'bashls',
+--     'clangd',
+--     'cmake',
+--     'dockerls',
+--     'eslint',
+--     'lua_ls',
+--     'marksman',
+--     'pyright',
+--     'tsserver',
+-- })
+
+-- Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
